43da5e39bcc71a23733bd59ba11b84a9283ad6e4
wisevaishu <vpm.win@gmail.com>
2024-07-11 14:42:46 -0500

    Added full login register

--- src/main/java/com/example/RegisterLogin/Config/SecurityConfig.java
+++ src/main/java/com/example/RegisterLogin/Config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.RegisterLogin.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
@@ -11,31 +12,45 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig  implements WebMvcConfigurer {
+@EnableWebMvc
+public class SecurityConfig   implements WebMvcConfigurer {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-           .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-            .csrf(AbstractHttpConfigurer::disable);
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/employee/login", "/api/v1/employee/save").permitAll() // Permit access to login and register endpoints
+                        .anyRequest().authenticated() // All other requests need to be authenticated
+                )
+
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(form -> form
+                        .loginPage("/login") // Custom login page (if you have one)
+                        .permitAll()
+                );
+
         return http.build();
-   }
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                //.allowedOrigins("http://localhost:8082")
                 .allowedOrigins("http://localhost:8082")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
+
     }
 }
